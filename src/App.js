24ec658662612e630f9cc7974979a0ef73aa2266
Luan Leal <luanlealboni@gmail.com>
2022-09-14 11:40:54 -0300

    Rotas Criadas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Login from "./Components/Login.js"
-import GlobalStyle from "./Styles/GlobalStyles.js"
+import Login from "./Components/Login.js";
+import GlobalStyle from "./Styles/GlobalStyles.js";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 
 
 export default function App(){
     return(
         <>
         <GlobalStyle/>
-        <Login />
+        <BrowserRouter>
+        <Routes>
+        <Route path="/login" element={<Login />}/>
+        </Routes>
+        </BrowserRouter>
         </>
         )
     
