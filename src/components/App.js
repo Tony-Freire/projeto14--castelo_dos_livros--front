46986aaab44a612cf63e302ba0435d407c5437c7
Luan Leal <luanlealboni@gmail.com>
2022-09-15 08:51:19 -0300

    Merge entre os arquivos de login e os de home

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import GlobalStyle from "./../Styles/GlobalStyles.js";
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import HomePage from "./homePage/HomePage";
-
+import Login from "./login/Login.js";
 
 export default function App()
 {
@@ -11,6 +11,7 @@ export default function App()
         <BrowserRouter>
         <Routes>
         <Route path="/" element={<HomePage />}/>
+        <Route path="/login" element={<Login />}/>
         </Routes>
         </BrowserRouter>
         </>
